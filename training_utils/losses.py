9f9e92682e8a059c9ff47f801244691fe0d5f52b
Morteza Pasandi <morteza.mmp1375@gmail.com>
2022-04-14 14:32:17 -0400

    Update losses.py
    adding the smooth loss v2 for the regression

--- training_utils/losses.py
+++ training_utils/losses.py
@@ -95,3 +95,34 @@ def smooth_l1_loss(sigma, reg_channels, data_format='channels_last'):
     compute_smooth_l1_loss.__name__ = 'smooth_l1'
 
     return compute_smooth_l1_loss
+def smooth_l1_lossV2(sigma, reg_channels, data_format='channels_last'):
+
+    sigma_squared = sigma ** 2
+
+    def compute_smooth_l1_loss(y_true, y_pred):
+
+
+        mask = tf.reshape(tf.equal(y_true, 1.0), shape=(-1, reg_channels)) 
+        pos_count = tf.reduce_sum(tf.cast(mask, tf.float32))
+
+        #y_true = y_true[...,:-1]
+
+        y_true_masked = tf.boolean_mask(tf.reshape(y_true, shape=(-1, reg_channels)), mask=mask, axis=0)
+        y_pred_masked = tf.boolean_mask(tf.reshape(y_pred,shape=(-1, reg_channels)), mask=mask, axis=0)                                
+
+        if y_true_masked.shape[0] == 0:
+            return tf.constant(value=0., dtype=tf.float32)
+
+        abs_diff  = tf.abs(y_true - y_pred)
+
+        smooth_l1 = tf.where(abs_diff < (1. / sigma_squared), 
+                             0.5 * sigma_squared * tf.square(abs_diff), 
+                             abs_diff - 0.5 / sigma_squared)
+
+        sum = tf.reduce_sum(smooth_l1, axis=1)
+        loss = tf.reduce_mean(sum)
+        return loss
+    
+    compute_smooth_l1_loss.__name__ = 'smooth_l1'
+
+    return compute_smooth_l1_loss
