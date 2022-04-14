fd7bf86bce4428c85c79a5dd8f24a253f23ad89c
Morteza Pasandi <morteza.mmp1375@gmail.com>
2022-04-14 14:34:46 -0400

    Update metrics.py

--- training_utils/metrics.py
+++ training_utils/metrics.py
@@ -44,3 +44,30 @@ def reg_metric(value_idx, reg_channels):
     # elif value_idx == 10:
     #     compute_metric.__name__ = 'ratio_log_h'
     return compute_metric
+def reg_metricV2(value_idx, reg_channels):
+
+    def compute_metricv2(y_true, y_pred):
+        mask =  tf.reshape(tf.equal(y_true[...,-1], 1.0), shape=(-1,))
+        pos_count = tf.reduce_sum(tf.cast(mask, tf.float32))
+
+        y_true_masked = tf.boolean_mask(tf.reshape(y_true, shape=(-1, reg_channels)), 
+                                        mask=mask, 
+                                        axis=0)
+        y_pred_masked = tf.boolean_mask( tf.reshape(y_pred, shape=(-1, reg_channels)),
+                                        mask=mask,
+                                        axis=0)
+
+        y_true = y_true_masked[:,value_idx]
+        y_pred = y_pred_masked[:,value_idx]
+
+        return tf.cond(tf.equal(tf.size(y_true), 0), lambda: tf.constant(0.0), lambda: tf.reduce_mean(tf.abs(y_true - y_pred))) 
+    
+    if value_idx == 0:
+        compute_metricv2.__name__ = 'x_offset'
+    elif value_idx == 1:
+        compute_metricv2.__name__ = 'y_offset'
+    elif value_idx == 2:
+        compute_metricv2.__name__ = 'log_w'
+    elif value_idx == 3:
+        compute_metricv2.__name__ = 'log_h'
+    return compute_metricv2    
